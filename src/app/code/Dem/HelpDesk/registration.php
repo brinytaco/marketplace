eb67254c22e54343a54f97d4cdacce9d0426638c
Toby Crain <tcrain@directedgemedia.com>
2021-04-06 11:28:47 -0700

    HelpDesk: Added module registration

---
+++ src/app/code/Dem/HelpDesk/registration.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Copyright © Direct Edge Media, Inc. All rights reserved.
+ */
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(ComponentRegistrar::MODULE, 'Dem_HelpDesk', __DIR__);
