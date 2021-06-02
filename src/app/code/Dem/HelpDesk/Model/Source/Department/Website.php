032a6a14ae37c60fe21bd45d5f290f25fd483bfc
Toby Crain <tcrain@directedgemedia.com>
2021-06-02 15:50:17 -0700

    HelpDesk: Added website to department grid

---
+++ src/app/code/Dem/HelpDesk/Model/Source/Department/Website.php
@@ -0,0 +1,20 @@
+<?php
+declare(strict_types=1);
+
+namespace Dem\HelpDesk\Model\Source\Department;
+
+use Dem\HelpDesk\Model\Source\SourceOptions;
+
+/**
+ * HelpDesk Source Model - Department Website
+ *
+ * =============================================================================
+ *
+ * @package    Dem\HelpDesk
+ * @copyright  Copyright (c) 2021 Direct Edge Media (http://directedgemedia.com)
+ * @author     Toby Crain
+ * @since      1.0.0
+ */
+class Website extends \Dem\HelpDesk\Model\Source\CaseItem\Website
+{
+}
